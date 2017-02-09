f574fecf4d6eeefc53a073064167f0cdcf7b92cd
Martin Craig <ctsu0221@dhcpibme43211.eng.ox.ac.uk>
2017-02-09 13:20:39 +0000

    Minor fixes

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,9 +10,11 @@ scriptdir=`dirname $0`
 $scriptdir/build.sh release
 cd $scriptdir/../build_release
 make package
+cd ..
 
 $scriptdir/build.sh debug
 cd $scriptdir/../build_debug
 make package
+cd ..
 
 cd $ORIGDIR
