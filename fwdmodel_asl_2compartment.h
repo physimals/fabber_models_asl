d5958725e43aec474a8cbf1df03c7d5e66cf021e
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-06-05 09:19:30 +0100

    Formatting only

--- fwdmodel_asl_2compartment.h
+++ fwdmodel_asl_2compartment.h
@@ -27,18 +27,24 @@ public:
     void GetOptions(std::vector<OptionSpec> &opts) const;
     std::string ModelVersion() const;
     std::string GetDescription() const;
-    void EvaluateModel(const NEWMAT::ColumnVector &params, NEWMAT::ColumnVector &result, const std::string &key="") const;
-    
+    void EvaluateModel(const NEWMAT::ColumnVector &params, NEWMAT::ColumnVector &result,
+        const std::string &key = "") const;
+
 protected:
     void GetParameterDefaults(std::vector<Parameter> &params) const;
 
 private:
-    enum SolutionType {DIST, SLOW, FAST};
+    enum SolutionType
+    {
+        DIST,
+        SLOW,
+        FAST
+    };
     SolutionType m_sol;
     bool m_infer_bat, m_infer_t1, m_infer_t1b, m_infer_t1e;
     double m_f_initial, m_sig0;
     double m_bat, m_t1, m_t1b, m_t1e, m_ie, m_bolusdur, m_ps, m_vb, m_vbw;
-    
+
     /** Time values of the data volumes - must match number of volumes in data */
     std::vector<double> m_times;
 
