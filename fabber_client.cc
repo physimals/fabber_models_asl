1889c0904fa814fdac13821257a21263227ed921
Martin Craig <martin.craig@nottingham.ac.uk>
2023-02-06 10:06:33 +0000

    Add newinstance calls to work around factory registration broken in build against FSL6.0.6

--- fabber_client.cc
+++ fabber_client.cc
@@ -6,9 +6,33 @@
 
 /*  CCOPYRIGHT */
 
-#include "fabber_core/fabber_core.h"
+#include <fabber_core/fabber_core.h>
+
+#include "fwdmodel_asl_2compartment.h"
+#include "fwdmodel_asl_grase.h"
+#include "fwdmodel_asl_multiphase.h"
+#include "fwdmodel_asl_quasar.h"
+#include "fwdmodel_asl_rest.h"
+#include "fwdmodel_asl_satrecov.h"
+#include "fwdmodel_asl_satrecovdualfa.h"
+#include "fwdmodel_asl_turboquasar.h"
+#include "fwdmodel_asl_multite.h"
+#include "fwdmodel_asl_velocityselective.h"
 
 int main(int argc, char **argv)
 {
+    // This should not be necessary! They are supposed to auto-register
+    // but this is not working in current build against FSL 6.0.6
+    // Possibly side-effect of move to shared libraries vs static
+    ASLFwdModel::NewInstance();
+    GraseFwdModel::NewInstance();
+    MultiPhaseASLFwdModel::NewInstance();
+    QuasarFwdModel::NewInstance();
+    TurboQuasarFwdModel::NewInstance();
+    ASL2CompartmentModel::NewInstance();
+    SatrecovFwdModel::NewInstance();
+    multiTEFwdModel::NewInstance();
+    VelocitySelectiveFwdModel::NewInstance();
+
     return execute(argc, argv);
 }
